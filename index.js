6fd8c7f4ed6c23abf8b37816cb420d4038c27a70
Paul <pt2323@gmail.com>
2015-02-28 17:40:26 -0500

    fix tabs

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ var DEFAULT_TABLE_PREFIX = 'sql_comment_'
 * options should include: knex (knex object), tablePrefix(defaults to "sql_comment")
 */
 module.exports = function(options, callback){
-	var self = this
+    var self = this
 
-	var commentTable
-	var voteTable
+    var commentTable
+    var voteTable
 
 /*******************************************************************************
 
-					INIT
-
+                    INIT
+    
 *******************************************************************************/
 
-	this.init = function(){
-		self.knex = options.knex
+    this.init = function(){
+        self.knex = options.knex
 
-		var prefix = DEFAULT_TABLE_PREFIX
-		if( options.tablePrefix ){
-			prefix = options.tablePrefix
-		}
+        var prefix = DEFAULT_TABLE_PREFIX
+        if( options.tablePrefix ){
+            prefix = options.tablePrefix
+        }
 
-		commentTable = prefix + 'comment'
-		voteTable = prefix + 'vote'
+        commentTable = prefix + 'comment'
+        voteTable = prefix + 'vote'
 
-		// init tables
-		require('./schema')(commentTable, voteTable, self.knex, callback)
-	}
+        // init tables
+        require('./schema')(commentTable, voteTable, self.knex, callback)
+    }
 
-	this.init()
+    this.init()
 }
